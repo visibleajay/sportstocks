8c1c166ad04a54d47e8f83243e1a24e42c0796c1
Ajay <ratixind@gmail.com>
2022-08-04 15:51:52 +0530

    Add logout to app.

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,10 +1,19 @@
+import { Form } from "@remix-run/react";
+
 import Header from "./stocks/header";
 
 export default function MyProfile() {
   return (
     <>
       <Header />
-      <div>Profile 1234</div>
+      <Form action="/logout" method="post">
+        <button
+          type="submit"
+          className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+        >
+          Logout
+        </button>
+      </Form>
     </>
   );
 }
