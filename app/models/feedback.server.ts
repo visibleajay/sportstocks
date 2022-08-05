d0329862bbb1194a21de2af897c1c391bdf06926
Ajay <ratixind@gmail.com>
2022-08-05 15:08:58 +0530

    update types.

--- app/models/feedback.server.ts
+++ app/models/feedback.server.ts
@@ -4,6 +4,6 @@ import { prisma } from "~/db.server";
 export type { Feedback } from "@prisma/client";
 
 
-export function createFeedback({email, feedback, userId}: {email: String, feedback: String, userId: String}) {
+export function createFeedback({email, feedback, userId}: {email: string, feedback: string, userId: string}) {
     return prisma.feedback.create({data: {email, feedback, userId}});
 }
